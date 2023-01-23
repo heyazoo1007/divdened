9768919c58c00837432c4396201378165096e645
heyazoo1007 <heyazoo1007@gmail.com>
2023-01-23 20:56:13 +0900

    [feat] 배당금 프로젝트

---
+++ src/main/java/com/zerobase/dividend/exception/CustomException.java
@@ -0,0 +1,4 @@
+package com.zerobase.dividend.exception;
+
+public class CustomException {
+}
---
+++ src/main/java/com/zerobase/dividend/persist/CompanyRepository.java
@@ -0,0 +1,19 @@
+package com.zerobase.dividend.persist;
+
+import com.zerobase.dividend.persist.entity.CompanyEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
+
+    boolean existsByTicker(String ticker);
+
+    Optional<CompanyEntity> findByName(String name);
+
+    Optional<CompanyEntity> findByTicker(String ticker);
+
+    Page<CompanyEntity> findByNameStartingWithIgnoreCase(String s, Pageable pageable);
+}
